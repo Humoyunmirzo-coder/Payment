2e4e0644ea9f3a2b966fa72bd98787be30c61078
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2023-12-13 14:57:40 +0500

    Add project files.

---
+++ Aplication/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Repository
+{
+	public  interface IRepository <T>
+	{
+		Task<IEnumerable<T>> GetAllAsync();
+		Task<T> GetByIdAsync(int Id);
+	}
+}
---
+++ Aplication/Service/IUserAccaountService.cs
@@ -0,0 +1,17 @@
+﻿using Aplication.Repository;
+using Payment.Domain.Enititys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Service
+{
+	public  interface IUserAccaountService : IRepository<UserAccount>
+ 	{
+		Task <string> GetUserAccamountAsync (string username, string password);
+		Task<IEnumerable<object>> GetById();
+		
+	}
+}
